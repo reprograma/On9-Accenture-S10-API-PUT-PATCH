3cf4b73a34981990e7074e968d7894d4dba0410c
Denise <deniseevelynk3@hotmail.com>
2020-10-16 23:57:31 -0300

    Add PUT e PATCH

--- src/controllers/postagens-controllers.js
+++ src/controllers/postagens-controllers.js
@@ -1,5 +1,6 @@
 const postagensModel = require("../moldels/postagens-models");
 const helper = require("../helpers/helpers");
+const { request, response } = require("express");
 
 const todasPostagens = (request, response) => {
     response.status(200).json(postagensModel);
@@ -28,6 +29,43 @@ const criarPostagem = (request, response) => {
     response.status(201).json(novaPostagem);
 }
 
+const atualizarPostagem = (request, response) => {
+    const {id} = request.params;
+    const filtrarPostagem = postagensModel.filter(postagem => {
+        return postagem.id == id;
+    }) [0];
+
+    const index = postagensModel.indexOf(filtrarPostagem);
+
+    const obterChaves = Object.keys(request.body);
+    obterChaves.forEach( chave => {
+        filtrarPostagem[chave] = request.body[chave];
+    })
+    postagensModel[index]  = filtrarPostagem;
+
+    response.status(200).json(postagensModel[index]);
+}
+
+const atualizarTituloPostagem = (request, response) => {
+    const {id} = request.params;
+    const {titulo} = request.body;
+    const postagem = postagensModel.find(postagem => postagem.id == id);
+
+    postagem.titulo = titulo;
+
+    response.status(204).json({mensagem: `O título foi atualizado com sucesso`});
+}
+
+const atualizarTagPostagem = (request, response) => {
+    const {id} = request.params;
+    const {tag} = request.body;
+    const postagem = postagensModel.find(postagem => postagem.id == id);
+
+    postagem.tag = tag;
+
+    response.status(204).json({mensagem: `A tag foi atualizada com sucesso`});
+}
+
 const deletarPostagem = (request, response) => {
     const {id} = request.params;
 
@@ -46,5 +84,8 @@ module.exports = {
     todasPostagens,
     getByTitle,
     criarPostagem,
+    atualizarPostagem,
+    atualizarTagPostagem,
+    atualizarTituloPostagem,
     deletarPostagem
 }
